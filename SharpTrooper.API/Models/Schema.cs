51f2b76fdfbc5df72d93566a6293c5bcf495ed7c
Joshua Kennedy <joshkennede@gmail.com>
2023-05-01 19:52:22 -0700

    Added schema poco

---
+++ SharpTrooper.API/Models/Schema.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SharpTrooper.API.Models
+{
+    public class Schema
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public List<string> renders { get; set; }
+        public List<string> parses { get; set; }
+    }
+}
